7e2b7bd998b08d56ad78fda46591eac20e4ea09e
nelab <naoufal.el.abouyi@pwc.com>
2021-09-27 08:18:47 +0200

    update resource

--- src/main/java/org/nel/ab/controller/DeviceResourceProvider.java
+++ src/main/java/org/nel/ab/controller/DeviceResourceProvider.java
@@ -1,10 +1,9 @@
 package org.nel.ab.controller;
 
-import org.keycloak.models.RealmModel;
-import org.nel.ab.dto.DeviceDto;
-import org.nel.ab.spi.DeviceService;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.resource.RealmResourceProvider;
+import org.nel.ab.dto.DeviceDto;
+import org.nel.ab.spi.DeviceService;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,6 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeviceResourceProvider implements RealmResourceProvider {
 
@@ -29,23 +30,24 @@ public class DeviceResourceProvider implements RealmResourceProvider {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String get() {
+    public List<DeviceDto> get() {
 //        String name = session.getContext().getRealm().getDisplayName();
 //        if (name == null) {
 //            name = session.getContext().getRealm().getName();
 //        }
-        DeviceService service = session.getProvider(DeviceService.class);
-
+//        DeviceService service = session.getProvider(DeviceService.class);
+        List<DeviceDto> result = new ArrayList<>();
+        result.add(DeviceDto.builder().brand("Apple").model("iphone").status("valid").build());
 //        List<DeviceDto> result = session.getProvider(DeviceService.class).listDevices();
 //        if(result==null || result.size()==0){
 //            result = new ArrayList<>();
 //        }
-        return service.toString();
+        return result;
     }
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response enroll(DeviceDto dto) {
-        session.getProvider(DeviceService.class).addDevice(dto);
+//        session.getProvider(DeviceService.class).addDevice(dto);
 //        String name = session.getContext().getRealm().getDisplayName();
 //        if (name == null) {
 //            name = session.getContext().getRealm().getName();
